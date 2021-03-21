3a5be83401dbe607c3c57573822934e095c2f3d1
Palash A <palash.agarwal.96@gmail.com>
2021-03-21 12:06:35 -0400

    add sample data

---
+++ sample-data/sample-data.swift
@@ -0,0 +1,42 @@
+[
+  CardModel(
+    title: "The world if..", 
+    short: "imagine the world if everything was actually web3!", 
+    long: "web2 vs web3 has a fundamental difference - who is in charge of data. web2 is a hangover of the colonial past wherein one individual or entity holds power over another. web3 on the other hand, gives back control, and thus power, to the user. what if the world was designed with the core assumption of web3, ie, an individual is sophisticated enough to manage their own resources and every action taken by them (or others) is an immutable change in society with some cost and repurcursions? what if the world was indeed completely connected and there were no need for middle men? what if all knowledge of the world was publically accessible without outrageous prerequisites? what if due to this insane connectivity, true democracy is actually possible?", 
+    image: "idea-1", 
+    story: "tired of being taken for a ride. rides are not optional, but being in the driver's seat of the ride is. not presently however, where that option is increasingly obfuscated. hence, lets move to 3.0! come play this game and see what the world would be like!!", 
+    url: "https://github.com/the-world-if/the-world-if.github.io",
+    ask: "42069", 
+    token: "MATIC"
+  ),
+  CardModel(
+    title: "ethBier",
+    short: "a global community of beer aficionados",
+    long: "an app, because no great story begins with a desk. the app houses all the breweries in the world, they be willing [to join the network] ofc. the app houses the means to connect to your buddies. the app houses the paths to discover your tastes. the app houses the protocol of loyalty. the app houses fun times on the exchange. why do you not house the app?",
+    image: "idea-2", 
+    story: "began as a project in ethDenver, and now sits proudly on my todo list. the bounty asked for a way to discover the various breweries Colorado has to offer. the bounty also mentioned something along the lines of a 'discovery trail' and naturally, we got carried away. add to that breweries rewarding their regulars by a badge which earns! also, add a bar stock exchange such that on a night if Budweiser is in demand and Corona is not, the Corona may be half the price of the Budweiser! finally, add the ability to message other users and create plans to meet up! welp. the project never finished. it barely got off the ground. and now, here it rests as a nfi.",
+    url: "https://github.com/Sloth-Monsters", 
+    ask: "15351", 
+    token: "PRQ"
+  ),
+  CardModel(
+    title: "scan lAtEx",
+    short: "how wonderful would it be if scanned documents were converted to beautiful LaTeX?", 
+    long: "a simple scanner that scans a document/page, runs some OCR on it, courtesy of google, and converts the interpreted data to LaTeX code, finally creating the most beautiful PDF you have ever seen (or even deserved) from wild scribbles and doodles. this project is insanely hard owing to the complications of diagram-recognition. then there is also the matter of actually writing the LaTeX. and then programmatically writing LaTeX.", 
+    image: "idea-3", 
+    story: "i had a professor who was brilliant but with terrible handwriting. due to his brilliance, he'd be always occupied and only make rough notes for a lecture, teaching from heart rather than rote. when asked by students to upload lectures for later reference, he'd simply scan and upload, watermarks and all. the agony of going through those notes sets the standard for this project, which someday will be completed. either by me, or somebody else.", 
+    url: "none-yet", 
+    ask: "62", 
+    token: "ETH"
+  ),
+  CardModel(
+    title: "i-need-a-break",
+    short: "a small cli tool to play simple arcade games when the compiler won't shut the fuck up.", 
+    long: "a collection of games available by the command line. games like connect4, tic-tac-toe, chess, scrabble, etc. typically play against an ai, or if feeling adventurous (or lonely) play against another human. online!", 
+    image: "idea-4", 
+    story: "in a class for ai, i was building a bot to play connect4. when finally it started working after about a month of dumb and annoying errors i wished for a game to play. one that would actually work and genuinely bring delight. one that would make me marvel at the wonders of cs. one that would make me want to go back into those dumb and annoying errors. and here we are.", 
+    url: "https://github.com/thisispalash/i-need-a-break", 
+    ask: "1", 
+    token: "USDT"
+  )
+]
